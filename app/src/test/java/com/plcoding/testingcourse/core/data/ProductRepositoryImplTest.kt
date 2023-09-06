3e6646bebc99610743579464b7e2a0d0763bae71
noner <pawel495@interia.pl>
2023-09-06 19:44:05 +0100

    MockWebServer - a way of testing how the api reponses are handled

--- app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
@@ -1,7 +1,9 @@
 package com.plcoding.testingcourse.core.data
 
+import assertk.assertFailure
 import assertk.assertThat
 import assertk.assertions.isTrue
+import com.google.gson.Gson
 import com.plcoding.testingcourse.core.domain.AnalyticsLogger
 import com.plcoding.testingcourse.core.domain.LogParam
 import com.plcoding.testingcourse.core.domain.Product
@@ -12,21 +14,32 @@ import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import retrofit2.HttpException
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 internal class ProductRepositoryImplTest {
 
     private lateinit var repository: ProductRepositoryImpl
     private lateinit var productApi: ProductApi
     private lateinit var analyticsLogger: AnalyticsLogger
+    private lateinit var mockWebServer: MockWebServer
 
     @BeforeEach
     fun setUp() {
-        productApi = mockk()
+        mockWebServer = MockWebServer()
+        productApi = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(mockWebServer.url("/"))
+            .build()
+            .create()
         analyticsLogger = mockk(relaxed = true)
         repository = ProductRepositoryImpl(productApi, analyticsLogger)
     }
@@ -47,6 +60,26 @@ internal class ProductRepositoryImplTest {
                 "http_error",
                 LogParam("code", 404),
                 LogParam("message", "Test message"),
+            )
+        }
+    }
+
+    @Test
+    fun `Response error, exception logged - MockWebServer`() = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse().setResponseCode(404)
+        )
+
+        val result = repository.purchaseProducts(listOf())
+
+
+        assertThat(result.isFailure).isTrue()
+
+        verify {
+            analyticsLogger.logEvent(
+                "http_error",
+                LogParam("code", 404),
+                LogParam("message", "Client Error")
             )
         }
     }
